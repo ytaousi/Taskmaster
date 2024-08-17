4cd71d9d44744542e1eec2dee05879d11d20560f
ytaousi <yassir.taous@gmail.com>
2024-08-17 13:12:14 -0400

    .. HoOo

---
+++ supervisord/main.py
@@ -0,0 +1,29 @@
+from multiprocessing import Process
+import os
+
+
+class Taskmaster(Process):
+    def run(self):
+        print('module name:', __name__)
+        print('parent process:', os.getppid())
+        print('process id:', os.getpid())
+    def __init__(self, value):
+        Process.__init__(self)
+        self.value = value   
+
+def info(title):
+    print(title)
+    print('module name:', __name__)
+    print('parent process:', os.getppid())
+    print('process id:', os.getpid())
+
+def f(name):
+    info('function f')
+    print('hello', name)
+
+if __name__ == '__main__':
+    info('main line')
+    process = Process(target=f, args=('bob',))
+    process.start()
+    process.join()
+    
