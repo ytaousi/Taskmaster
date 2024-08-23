aac962b9476eca63246323492e6c1e54841a5e82
ytaousi <yassir.taous@gmail.com>
2024-08-23 06:32:06 +0100

    42 The Network becoming 1337 The Point of View

--- supervisord/logHandler.py
+++ supervisord/logHandler.py
@@ -1,11 +1,13 @@
 # only standard libraries are used to build the supervisord logging management system.
 import sys
 import os
+import time
 
 class supervisorLogHandler:
     def __init__(self, args):
         self.args = args
-        self.supervisorLogFilePath = ""
+        self.supervisorLogFilePath = "/home/normal-user/Desktop/Taskmaster/logs/supervisord_test.log"
+        self.destinations = {}
 
     def print_config(self):
         pass
@@ -24,6 +26,17 @@ class supervisorLogHandler:
     def writeStdOutLogs(self):
         pass
 
+    def add_destionation(self, log_type, destination):
+        if log_type not in self.destionations:
+            self.destionations[log_type] = []
+        self.destionations[log_type].append(destination)
+
+    def logsFormatter(self, message, log_type="supervisord", level="INFO"):
+        timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        formattedMessage = f"{timestamp} [{level}] {message}"
+        if log_type in self.destionations:
+            for dest in self.destionations[log_type]:
+                dest.write(formattedMessage)
 
 def main():
     supervisorloghandler = supervisorLogHandler(args=[
